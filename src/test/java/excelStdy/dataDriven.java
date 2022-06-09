d0e4b5dff5b327988ab1bedef19851e0c9caa1b4
Andrew <kubyk.a@gmail.com>
2022-06-09 15:39:55 -0500

    log4j added

--- src/test/java/excelStdy/dataDriven.java
+++ src/test/java/excelStdy/dataDriven.java
@@ -22,71 +22,73 @@ public class dataDriven {
         ArrayList<String> a=new ArrayList<String>();
         // fileInputStream argument\\ create an object that will read testcase file
         FileInputStream fis = new FileInputStream("/Users/andrew/dev0/files/testcases1.xlsx");
-        //create object for class XSSWorkbook
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
+        try (//create object for class XSSWorkbook
+        XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
+            //count number of sheets
+            int sheets = workbook.getNumberOfSheets();
+            
+            //itereate though file and find necessary sheet
+            for(int i=0; i<sheets; i++){
+                if (workbook.getSheetName(i).equalsIgnoreCase("sheet1")){
+                    //get acess to sheet
+                    XSSFSheet sheet =  workbook.getSheetAt(i);
 
-        //count number of sheets
-        int sheets = workbook.getNumberOfSheets();
-        
-        //itereate though file and find necessary sheet
-        for(int i=0; i<sheets; i++){
-            if (workbook.getSheetName(i).equalsIgnoreCase("sheet1")){
-                //get acess to sheet
-                XSSFSheet sheet =  workbook.getSheetAt(i);
-
-                //identify row 
-                Iterator<Row> rows = sheet.iterator();
-                //get acess to specific row
-                Row firstrow = rows.next();
-                //scan row to get each cell in that row
-                Iterator<Cell> ce = firstrow.cellIterator();
-                //counter variable
-                int k=0;
-                //column index varaible
-                int column=0;
-                //reach each cell
-                while(ce.hasNext()){
-                    //grab cell
-                    Cell value= ce.next();
-                    //get string present in that cell
-                    if(value.getStringCellValue()
-                        //check if this cell contains necessary text
-                        .equalsIgnoreCase("testcases")){
-                            //lock required row->
-                            //assign column = k. Because k will be 4 outside this while loop(number of all columns in the row)
-                            column=k;
+                    //identify row 
+                    Iterator<Row> rows = sheet.iterator();
+                    //get acess to specific row
+                    Row firstrow = rows.next();
+                    //scan row to get each cell in that row
+                    Iterator<Cell> ce = firstrow.cellIterator();
+                    //counter variable
+                    int k=0;
+                    //column index varaible
+                    int column=0;
+                    //reach each cell
+                    while(ce.hasNext()){
+                        //grab cell
+                        Cell value= ce.next();
+                        //get string present in that cell
+                        if(value.getStringCellValue()
+                            //check if this cell contains necessary text
+                            .equalsIgnoreCase("testcases")){
+                                //lock required row->
+                                //assign column = k. Because k will be 4 outside this while loop(number of all columns in the row)
+                                column=k;
+                        }
+                        k++;
                     }
-                    k++;
-                }
-                System.out.println(column);
+                    System.out.println(column);
 
-                //iterate throug rows
-                while(rows.hasNext()){
-                    //move coursor to nex row
-                    Row r = rows.next();
-                    //find required row
-                    if(r.getCell(column).getStringCellValue().equalsIgnoreCase(testcaseName)){
-                        //iterate throug cells in that row
-                        Iterator<Cell> cv= r.cellIterator();
-                        while(cv.hasNext()){
-                            Cell c = cv.next();
-                            if(c.getCellType()==CellType.STRING){
-                                //grab value of current cell and add to arraylist
-                                a.add(c.getStringCellValue());
-                            }
-                            else{
-                               a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
-                                
-                            }
-                            //
+                    //iterate throug rows
+                    while(rows.hasNext()){
+                        //move coursor to nex row
+                        Row r = rows.next();
+                        //find required row
+                        if(r.getCell(column).getStringCellValue().equalsIgnoreCase(testcaseName)){
+                            //iterate throug cells in that row
+                            Iterator<Cell> cv= r.cellIterator();
+                            while(cv.hasNext()){
+                                Cell c = cv.next();
+                                if(c.getCellType()==CellType.STRING){
+                                    //grab value of current cell and add to arraylist
+                                    a.add(c.getStringCellValue());
+                                }
+                                else{
+                                   a.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+                                    
+                                }
+                                //
 
+                            }
                         }
                     }
-                }
 
+                }
             }
         }
+
         return a;
+        
 
     }
 
