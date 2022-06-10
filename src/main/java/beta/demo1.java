10aa65c903bd4b184c9876258f823b385febc7b7
Andrew <kubyk.a@gmail.com>
2022-06-10 12:58:40 -0500

    logging1

---
+++ src/main/java/beta/demo1.java
@@ -0,0 +1,17 @@
+package beta;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class demo1 {
+
+    private static Logger log = LogManager.getLogger(demo1.class.getName());
+
+    public static void main(String[] args) {
+        log.debug("I am debugging");
+        
+            log.info("info message");
+        log.error("error message");
+    }
+    
+}
